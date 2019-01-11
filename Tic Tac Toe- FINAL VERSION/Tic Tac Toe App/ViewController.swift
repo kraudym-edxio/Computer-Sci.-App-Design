64211de7c1408033c0557716fbf7a8d42b1e770f
kraudym-edxio <46289177+kraudym-edxio@users.noreply.github.com>
2019-01-10 22:07:40 -0500

    Add files via upload

---
+++ Tic Tac Toe- FINAL VERSION/Tic Tac Toe App/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe App
+//
+//  Created by Edxio Kraudy on 2018-11-28.
+//  Copyright © 2018 Edxio Kraudy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    var activePlayer = 1 //Cross
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    let winningCombinations = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
+    var gameIsActive = true
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func action(_ sender: UIButton)
+    {
+        if (gameState[sender.tag-1] == 0 && gameIsActive == true)
+        {
+            gameState[sender.tag-1] = activePlayer
+            
+            if (activePlayer == 1)
+            {
+                sender.setImage(UIImage(named :"Cross.png"), for: UIControl.State())
+                activePlayer = 2
+            }
+            else
+            {
+                sender.setImage(UIImage(named :"Nought.png"), for: UIControl.State())
+                activePlayer = 1
+            }
+        }
+        
+        for combination in winningCombinations
+        {
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]
+            {
+                gameIsActive = false
+                
+                if gameState[combination[0]] == 1
+                {
+                    label.text = "CROSS HAS WON!"
+                }
+                else
+                {
+                    label.text = "NOUGHT HAS WON!"
+                }
+                
+                playAgainButton.isHidden = false
+                label.isHidden = false
+            }
+        }
+        
+        var count = 1
+    
+        if gameIsActive == true
+        {
+            for i in gameState
+            {
+                count = i*count
+            }
+            if count != 0
+            {
+                label.text = "IT WAS A DRAW!"
+                label.isHidden = false
+                playAgainButton.isHidden = false
+            }
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBAction func playAgain(_ sender: Any)
+    {
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        gameIsActive = true
+        activePlayer = 1
+        
+        playAgainButton.isHidden = true
+        label.isHidden = true
+        
+        for i in 1...9
+        {
+            let button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: UIControl.State())
+        }
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
